9e88f73c09c0c694ac536f864251bd9644ac5808
maskunandrianto <maskunandrianto@gmail.com>
2023-05-05 18:56:52 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+//  eventpada saat link di klik
+
+$('.page-scroll').on('click', function(e){
+
+	// ambil isi href
+	var tujuan = $(this).attr('href');
+	
+	// pindahkan scroll
+	var elemenTujuan = $(tujuan);
+
+
+	$('html,body').animate({
+		scrollTop: elemenTujuan.offset().top - 50
+	}, 1000, 'swing');
+
+
+	e.preventDefault();
+
+});
+
+// About 
+$(window).on('load', function() {
+	$('.pKiri').addClass('pMuncul');
+	$('.pKanan').addClass('pMuncul');
+});
+
+// Parralax  
+$(window).scroll(function() {
+
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1 +'%)'
+	});
+
+
+	// portofolio
+
+	if( wScroll > $('.portfolio').offset().top - 250 ) {
+		$('.portfolio .thumbnail').each(function(i) {
+			setTimeout(function() {
+				$('.portfolio .thumbnail').eq(i).addClass('muncul');
+			}, 300 * (i+1));
+		});
+	}
+})
+
